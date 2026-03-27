cce85c54b96fb851316de5ffca3cab8e75905d4b
AbbyCami08 <abbycami0805@gmail.com>
2026-03-26 21:11:38 -0600

    Lab 11

---
+++ src/main/java/com/techShop/tienda/repository/RolRepository.java
@@ -0,0 +1,15 @@
+package com.techShop.tienda.repository;
+
+import com.techShop.tienda.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author abbyc
+ */
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+    
+}
