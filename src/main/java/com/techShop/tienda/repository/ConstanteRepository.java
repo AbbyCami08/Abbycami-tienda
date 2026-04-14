1d2682e58c15880e85f475943ecc887dced265e8
AbbyCami08 <abbycami0805@gmail.com>
2026-04-13 20:31:02 -0600

    Lab 13

---
+++ src/main/java/com/techShop/tienda/repository/ConstanteRepository.java
@@ -0,0 +1,15 @@
+package com.techShop.tienda.repository;
+
+import com.techShop.tienda.domain.Constante;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author abbyc
+ */
+public interface ConstanteRepository extends JpaRepository<Constante,Integer> {
+    
+    public Optional<Constante> findByAtributo(String atributo);
+    
+}
